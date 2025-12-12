c8d1ab528555d3ebf9c40d1a75820b72c0fe3181
Prateek Kumar <102872770+Prateek-create@users.noreply.github.com>
2025-12-12 11:31:11 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,3 +14,29 @@ sudo apt install flatpak -y
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub org.chromium.Chromium -y
 
+sudo apt install -y \
+  libssl3 libssl-dev libgstreamer1.0-0 gstreamer1.0-tools \
+  gstreamer1.0-plugins-good gstreamer1.0-plugins-bad \
+  gstreamer1.0-plugins-ugly gstreamer1.0-libav \
+  libgstreamer-plugins-base1.0-dev libgstrtspserver-1.0-0 \
+  libjansson4 libyaml-cpp-dev libjsoncpp-dev protobuf-compiler \
+  gcc make git python3
+
+git clone https://github.com/confluentinc/librdkafka.git
+cd librdkafka
+git checkout tags/v2.2.0
+./configure --enable-ssl
+make
+sudo make install
+sudo mkdir -p /opt/nvidia/deepstream/deepstream/lib
+sudo cp /usr/local/lib/librdkafka* /opt/nvidia/deepstream/deepstream/lib
+sudo ldconfig
+
+wget --content-disposition 'https://api.ngc.nvidia.com/v2/resources/org/nvidia/deepstream/7.0/files?redirect=true&path=deepstream-7.0_7.0.0-1_arm64.deb' --output-document 'deepstream-7.0_7.0.0-1_arm64.deb'
+
+sudo apt-get install ./deepstream-7.0_7.0.0-1_arm64.deb
+sudo ldconfig
+
+/opt/nvidia/deepstream/deepstream-7.0/bin/deepstream-app --version
+
+sudo reboot
